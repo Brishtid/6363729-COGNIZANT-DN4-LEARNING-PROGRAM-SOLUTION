36b49eafc90c6e2effb3cdafcfe13b24d8aa1810
Brishtid <145860884+Brishtid@users.noreply.github.com>
2025-07-05 19:44:48 +0530

    Add files via upload

---
+++ Week3-EntityFramework/3_RetailCLI_create/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using _3_RetailCLI.Data;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        //6363729
+        services.AddDbContext<RetailDbContext>(options =>
+            options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RetailInventoryQn3Db"));
+    });
+
+var app = builder.Build();
+Console.WriteLine("EF Core CLI project is configured.");
