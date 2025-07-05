44a8cd7b6c538dfb6bd8d1bf30918cc7df79325a
Brishtid <145860884+Brishtid@users.noreply.github.com>
2025-07-05 19:49:48 +0530

    Add files via upload

---
+++ Week3-EntityFramework/4_InsertIntoDB/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace _4_InsertIntoDB.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
