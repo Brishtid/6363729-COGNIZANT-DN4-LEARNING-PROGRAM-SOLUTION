a54998a103d434842a7f6f1ef064c307c876b781
Brishtid <145860884+Brishtid@users.noreply.github.com>
2025-07-05 19:30:52 +0530

    Add files via upload

---
+++ Week3-EntityFramework/1_RetailInventory/Program.cs
@@ -0,0 +1,39 @@
+﻿using RetailInventory.Data;
+using RetailInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+using var context = new AppDbContext();
+
+
+context.Database.EnsureCreated();
+
+// brishti
+if (!context.Categories.Any())
+{
+    var electronics = new Category { Name = "Electronics" };
+    var groceries = new Category { Name = "Groceries" };
+
+    context.Categories.AddRange(electronics, groceries);
+
+    context.Products.AddRange(
+        new Product { Name = "Laptop", QuantityInStock = 10, Category = electronics },
+        new Product { Name = "Smartphone", QuantityInStock = 25, Category = electronics },
+        new Product { Name = "Rice", QuantityInStock = 100, Category = groceries }
+    );
+
+    context.SaveChanges();
+    Console.WriteLine("Sample data inserted.");
+}
+else
+{
+    Console.WriteLine("Categories already exist.");
+}
+
+//6363729
+var products = context.Products.Include(p => p.Category).ToList();
+
+foreach (var p in products)
+{
+    Console.WriteLine($"{p.Name} - {p.Category.Name} - Stock: {p.QuantityInStock}");
+}
