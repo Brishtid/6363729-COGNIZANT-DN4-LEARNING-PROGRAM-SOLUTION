807b666353271f355dd6781ae61d5104d9e53b4c
Brishtid <145860884+Brishtid@users.noreply.github.com>
2025-07-14 01:41:54 +0530

    Add files via upload

---
+++ Week 4 ASP.NET Core WebApi/3_webapi/3.2Filter Authorization/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+
+namespace _2_WebApiEmployee.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            string logMessage = $"[{DateTime.Now}] Exception: {context.Exception.Message}\nStackTrace: {context.Exception.StackTrace}\n\n";
+
+            
+            File.AppendAllText("C:\\Logs\\WebApiErrorLog.txt", logMessage);
+
+            context.Result = new ObjectResult("Something went wrong.")
+            {
+                StatusCode = 500
+            };
+        }
+    }
+}
