8e247d3503e7033a41dce56d6a4d3d72cea49a95
Brishtid <145860884+Brishtid@users.noreply.github.com>
2025-07-13 16:44:19 +0530

    Add files via upload

---
+++ Week 4 ASP.NET Core WebApi/1 & 2 First Web Api/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/Emp")]  
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Brishti", "Anuj", "Asmi", "Anisha" };
+        }
+    }
+}
