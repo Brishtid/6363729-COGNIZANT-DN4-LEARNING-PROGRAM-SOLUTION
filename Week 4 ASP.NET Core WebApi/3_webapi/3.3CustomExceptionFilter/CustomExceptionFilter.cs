874003fc816775dad762c03f8cf924fb1d4fd7e6
Brishtid <145860884+Brishtid@users.noreply.github.com>
2025-07-14 01:59:56 +0530

    Add files via upload

---
+++ Week 4 ASP.NET Core WebApi/3_webapi/3.3CustomExceptionFilter/CustomExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+
+namespace _2_WebApiEmployee.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            string logPath = "C:\\Logs\\WebApiErrorLog.txt";
+
+            
+            Directory.CreateDirectory(Path.GetDirectoryName(logPath)!);
+
+            string error = $"[{DateTime.Now}] Exception: {context.Exception.Message}\nStackTrace: {context.Exception.StackTrace}\n\n";
+            File.AppendAllText(logPath, error);
+
+            context.Result = new ObjectResult("Something went wrong. Please try again later.")
+            {
+                StatusCode = 500
+            };
+        }
+    }
+}
---
+++ Week 4 ASP.NET Core WebApi/3_webapi/3.3CustomExceptionFilter/Program.cs
@@ -0,0 +1,27 @@
+﻿using _2_WebApiEmployee.Filters;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<CustomExceptionFilter>(); // ✅ Register globally
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//6363729
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run(); 
