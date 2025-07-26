91cb07bb13588b7a6e3799fbe22f55f17f200167
Brishtid <145860884+Brishtid@users.noreply.github.com>
2025-07-26 15:56:00 +0530

    Add files via upload

---
+++ WEEK 6 -REACT/2_student_app/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Welcome 6363729 to the Home page of Student Management Portal</h3>
+      </div>
+    );
+  }
+}
+
+export default Home;
